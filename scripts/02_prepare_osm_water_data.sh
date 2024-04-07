672454ce65bd62637622bf9fda55393186c92f96
Ivan Tsai <d7cd9256-5e92-4a18-be76-c2f4271b6571@cptsai.com>
2024-04-07 21:32:11 +0800

    add script for importing water data

---
+++ scripts/02_prepare_osm_water_data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+FILE_NAME="water-polygons-split-3857.zip"
+FOLDER_NAME="./raw-data"
+FILE_PATH=$FOLDER_NAME/$FILE_NAME
+
+if [ -f "$FILE_PATH" ]; then
+  echo "$FILE_PATH exists, skip download."
+else
+  echo "Downloading OSM water data..."
+  wget -O $FILE_PATH https://osmdata.openstreetmap.de/download/water-polygons-split-3857.zip
+fi
+
+unzip $FILE_PATH -d $FOLDER_NAME
+
+echo "Start import"
+ogr2ogr -f PostgreSQL PG:"dbname='osm' host='127.0.0.1' port='5432' user='postgres' password=''" $FOLDER_NAME/water-polygons-split-3857/water_polygons.shp -lco GEOMETRY_NAME=way -lco SPATIAL_INDEX=NONE -lco EXTRACT_SCHEMA_FROM_LAYER_NAME=YES -nln water -lco FID=osm_id -overwrite
+
+# Create index
+psql -p 5432 -h localhost -U postgres -c 'CREATE INDEX water_way ON water USING GIST (way);'
+
+rm -rf $FOLDER_NAME/water-polygons-split-3857
+echo "Done"
